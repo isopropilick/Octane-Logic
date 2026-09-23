8d3258d7c35d380e45602cd0c2dbe3e58dbcb5c7
Eric Pereyra <eric.pereyra@icloud.com>
2026-09-23 14:51:50 -0600

    fix: support C# 9 compilers

--- src/OctaneLogic.Core/Logic/LogicInputs.cs
+++ src/OctaneLogic.Core/Logic/LogicInputs.cs
@@ -1,20 +1,21 @@
-namespace OctaneLogic.Core.Logic;
-
-/// <summary>
-/// Normalized inputs for all Octane Logic nodes. Unused inputs are ignored by a node.
-/// </summary>
-public readonly struct LogicInputs
+namespace OctaneLogic.Core.Logic
 {
-    public LogicInputs(bool data, bool enable = false, bool set = false, bool reset = false)
+    /// <summary>
+    /// Normalized inputs for all Octane Logic nodes. Unused inputs are ignored by a node.
+    /// </summary>
+    public readonly struct LogicInputs
     {
-        Data = data;
-        Enable = enable;
-        Set = set;
-        Reset = reset;
-    }
+        public LogicInputs(bool data, bool enable = false, bool set = false, bool reset = false)
+        {
+            Data = data;
+            Enable = enable;
+            Set = set;
+            Reset = reset;
+        }
 
-    public bool Data { get; }
-    public bool Enable { get; }
-    public bool Set { get; }
-    public bool Reset { get; }
+        public bool Data { get; }
+        public bool Enable { get; }
+        public bool Set { get; }
+        public bool Reset { get; }
+    }
 }
